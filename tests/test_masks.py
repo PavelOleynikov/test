e93ec4893fc2055df7e5b5c5476478fde1321ace
Pavel Oleynikov <Pavlushka19805@gmail.com>
2025-08-15 14:38:58 +0400

    check lint

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -5,10 +5,7 @@ from src.masks import get_mask_account, get_mask_card_number
 
 @pytest.fixture
 def card_number():
-    return ["7000792289606361",
-            "700079228960",
-            ""
-]
+    return ["7000792289606361", "700079228960", ""]
 
 
 def test_get_mask_card_number_no_number():
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,9 +1,10 @@
 import pytest
 
-from src.widget import mask_account_card, get_date
+from src.widget import get_date, mask_account_card
 
 
-@pytest.mark.parametrize("input_data, expected_output",
+@pytest.mark.parametrize(
+    "input_data, expected_output",
     [
         # Тесты для карт (16 цифр)
         ("Maestro 1596837868705199", "Maestro 1596 83** **** 5199"),
@@ -21,29 +22,40 @@ def test_mask_account_card(input_data, expected_output):
 
 
 # Тесты для некорректных входных данных
-@pytest.mark.parametrize("invalid_input", [
-    None,
-    12345,
-    ["Visa", "1234567890123456"],
-    {"card": "1234567890123456"},
-])
+@pytest.mark.parametrize(
+    "invalid_input",
+    [
+        None,
+        12345,
+        ["Visa", "1234567890123456"],
+        {"card": "1234567890123456"},
+    ],
+)
 def test_non_string_input(invalid_input):
     with pytest.raises(ValueError, match="Input must be a string"):
         mask_account_card(invalid_input)
 
-@pytest.mark.parametrize("input_date, expected", [
-    ("2024-03-11T02:26:18.671407", "11.03.2024"),  # формат с временем
-    ("2024-03-11", "11.03.2024"),                 # Короткий формат
-    ("2024-12-31T23:59:59.999999", "31.12.2024"),  # Граничная дата
-])
+
+@pytest.mark.parametrize(
+    "input_date, expected",
+    [
+        ("2024-03-11T02:26:18.671407", "11.03.2024"),  # формат с временем
+        ("2024-03-11", "11.03.2024"),  # Короткий формат
+        ("2024-12-31T23:59:59.999999", "31.12.2024"),  # Граничная дата
+    ],
+)
 def test_valid_dates(input_date, expected):
     assert get_date(input_date) == expected
 
-@pytest.mark.parametrize("invalid_input", [
-    "",                           # Пустая строка
-    None,                         # None вместо строки
-    1234567890,                   # Число вместо строки
-])
+
+@pytest.mark.parametrize(
+    "invalid_input",
+    [
+        "",  # Пустая строка
+        None,  # None вместо строки
+        1234567890,  # Число вместо строки
+    ],
+)
 def test_invalid_dates(invalid_input):
     with pytest.raises(ValueError):
         get_date(invalid_input)
