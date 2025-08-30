a06a4b24612779dee956f05e6865ed5a272abf4e
Pavel Oleynikov <Pavlushka19805@gmail.com>
2025-08-30 16:44:17 +0400

    add functions in test_decorators and update decorators

--- src/decorators.py
+++ src/decorators.py
@@ -12,25 +12,33 @@ def log(filename=None):
                 result = func(*args, **kwargs)
                 if filename:
                     file = open(filename, "a", encoding="utf-8")
-                    file.write(f"{func.__name__} ок" + "\n")
+                    file.write(f"{func.__name__} ok" + "\n")
                     file.close()
                 else:
-                    print(f"{func.__name__} ок")
+                    print(f"{func.__name__} ok")
+                return result
+
             except Exception as e:
                 if filename:
-                    file = open(filename, "a", encoding="utf-8")
-                    file.write(f"{func.__name__} error: {str(e)}. Inputs: {args}, {kwargs}" + "\n")
+                    file = open(filename, "a", encoding="utf-8") #создаём и открываем файл логов
+                    file.write(f"{func.__name__} error: {type(e)}. Inputs: {args}, {kwargs}" + "\n")
                     file.close()
                 else:
-                    print(f"{func.__name__} error: {str(e)}. Inputs: {args}, {kwargs}")
+                    print(f"{func.__name__} error: {type(e)}. Inputs: {args}, {kwargs}")
                 raise
 
         return wrapper
     return decorator
 
 
-@log(filename="mylog.txt")
+@log() # вывод в консоль
 def my_function(x, y):
     return x + y
 
 my_function(1, 2)
+
+# @log(filename="mylog.txt") # запись в файл логов
+# def my_function(x, y):
+#     return x + y
+#
+# my_function("1", 2)
