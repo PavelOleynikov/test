74115d7a8b9406163a969321c16c2257cb3f428e
Pavel Oleynikov <Pavlushka19805@gmail.com>
2025-09-02 21:01:44 +0400

    add function get_data_operations in utils.py

---
+++ src/utils.py
@@ -0,0 +1,21 @@
+import json
+
+
+def get_data_operations(path: str) -> list:
+    """получает данные о финансовых транзакциях из json файла"""
+
+    try:
+        with open('../data/operations.json', 'r', encoding='utf-8') as file:
+            data = json.load(file)
+
+        if isinstance(data, list):
+            return data
+        else:
+            return []
+
+    except (FileNotFoundError, json.JSONDecodeError):
+        return []
+    except Exception:
+        return []
+
+print(get_data_operations('../data/operations.json'))
